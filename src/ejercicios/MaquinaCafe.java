8bcd68742a9707d405c5b5023b64f0359d2a6a44
Javi <javiersmr471@gmail.com>
2021-03-03 08:31:58 +0100

    Máquina de café avanzada

--- src/ejercicios/MaquinaCafe.java
+++ src/ejercicios/MaquinaCafe.java
@@ -1,18 +1,21 @@
 package ejercicios;
 
 public class MaquinaCafe {
-	private int MAXIMOCAFE=50;
-	private int MAXIMOLECHE=50;
-	private int MAXIMOVASOS=80;
-	private double preciocafe=1;
-	private double precioleche=0.8;
-	private double preciocafeconleche=1.5;
+	private final int MAXIMOCAFE=50;
+	private final int MAXIMOLECHE=50;
+	private final int MAXIMOVASOS=80;
+	private final double preciocafe=1;
+	private final double precioleche=0.8;
+	private final double preciocafeconleche=1.5;
 	private int depositocafe;
 	private int depositoleche;
 	private int depositovasos;
 	private double monedero;
-	public MaquinaCafe(double monedero) {
+	public MaquinaCafe(double monedero) throws Exception {
 		super();
+		if (monedero<0) {
+			throw new Exception("Para crear máquina introduzca cantidad positiva");
+		}
 		this.depositocafe = MAXIMOCAFE;
 		this.depositoleche = MAXIMOLECHE;
 		this.depositovasos = MAXIMOVASOS;
@@ -25,31 +28,31 @@ public class MaquinaCafe {
 	}
 	public double servicafe(double dinero) throws Exception {
 		if (monedero<dinero) {
-			throw new Exception();
+			throw new Exception("No tengo cambio para esa cantidad");
 		}
 		this.depositocafe--;
 		this.depositovasos--;
-		this.monedero=this.monedero+1;
-		return dinero-1;
+		this.monedero=this.monedero+preciocafe;
+		return dinero-preciocafe;
 	}
 	public double servirleche(double dinero) throws Exception {
 		if (monedero<dinero) {
-			throw new Exception();
+			throw new Exception("No tengo cambio para esa cantidad");
 		}
 		depositovasos--;
 		depositoleche--;
-		monedero=monedero+0.8;
-		return dinero-0.8;
+		monedero=monedero+precioleche;
+		return dinero-precioleche;
 	}
 	public double servircafeconleche(double dinero) throws Exception {
 		if (monedero<dinero) {
-			throw new Exception();
+			throw new Exception("No tengo cambio para esa cantidad");
 		}
 		depositovasos--;
 		depositoleche--;
 		depositocafe--;
-		monedero=monedero+1.5;
-		return dinero -1.5;
+		monedero=monedero+preciocafeconleche;
+		return dinero -preciocafeconleche;
 	}
 	public int getDepositocafe() {
 		return depositocafe;
@@ -66,6 +69,12 @@ public class MaquinaCafe {
 	}
 	public void setMonedero(double monedero) {
 		this.monedero = monedero;
+	}
+	@Override
+	public String toString() {
+		return "MaquinaCafe [preciocafe=" + preciocafe + ", precioleche=" + precioleche + ", preciocafeconleche="
+				+ preciocafeconleche + ", depositocafe=" + depositocafe + ", depositoleche=" + depositoleche
+				+ ", depositovasos=" + depositovasos + ", monedero=" + monedero + "]";
 	}
 	
 	
