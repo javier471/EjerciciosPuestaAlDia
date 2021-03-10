a1070e371f14c304c3a29cb5dd74535a65f4658f
Javi <javiersmr471@gmail.com>
2021-03-10 21:10:56 +0100

    Iniciada clase Producto

---
+++ src/Producto/Producto.java
@@ -0,0 +1,13 @@
+package Producto;
+
+public class Producto {
+	private final static double IVA=20;
+	private double precio;
+	private final static double codsigui=1;
+	private String desc; 
+	private int codigo=0;
+	
+
+	
+	
+}
