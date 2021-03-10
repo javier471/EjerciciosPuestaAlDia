9a35906c4042a681a251dcb65308971b5e860475
Javi <javiersmr471@gmail.com>
2021-03-10 19:28:51 +0100

    Linea corregida

--- src/Linea/Linea.java
+++ src/Linea/Linea.java
@@ -1,5 +1,4 @@
 package Linea;
-
 public class Linea {
 	private Punto lineaa;
 	private Punto lineab;
@@ -7,6 +6,11 @@ public class Linea {
 		super();
 		this.lineaa = lineaa;
 		this.lineab = lineab;
+	}
+	public Linea() {
+		super();
+		this.lineaa=new Punto(0,0);
+		this.lineab=new Punto(0,0);
 	}
 	public Punto getLineaa() {
 		return lineaa;
@@ -20,7 +24,51 @@ public class Linea {
 	public void setLineab(Punto lineab) {
 		this.lineab = lineab;
 	}
-	public void moverIzquierda() {
-		
+	public void moverDerecha(double x) {
+		this.lineaa.setCoorx(x+this.lineaa.getCoorx());
+		this.lineab.setCoorx(x+this.lineab.getCoorx());
 	}
+	public void moverIzquierda(double x) {
+		this.lineaa.setCoorx(x-this.lineaa.getCoorx());
+		this.lineab.setCoorx(x-this.lineab.getCoorx());
+	}
+	public void moverArriba(double y) {
+		this.lineaa.setCoory(y+this.lineaa.getCoorx());
+		this.lineaa.setCoory(y+this.lineaa.getCoory());
+		this.lineab.setCoory(y+this.lineab.getCoory());
+	}
+	public void moverAbajo(double y) {
+		this.lineaa.setCoory(y-this.lineaa.getCoory());
+		this.lineab.setCoory(y-this.lineab.getCoory());
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((lineaa == null) ? 0 : lineaa.hashCode());
+		result = prime * result + ((lineab == null) ? 0 : lineab.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Linea other = (Linea) obj;
+		if (lineaa == null) {
+			if (other.lineaa != null)
+				return false;
+		} else if (!lineaa.equals(other.lineaa))
+			return false;
+		if (lineab == null) {
+			if (other.lineab != null)
+				return false;
+		} else if (!lineab.equals(other.lineab))
+			return false;
+		return true;
+	}
+	
 }
