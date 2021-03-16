53cf036ae9ef17c630918a18340df697765f785d
Javi <javiersmr471@gmail.com>
2021-03-16 10:05:01 +0100

    Ejer binario

---
+++ EjercicioString/EjerBinario/EjerBinario.java
@@ -0,0 +1,17 @@
+package EjerBinario;
+
+public class EjerBinario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String aux="1011";
+		System.out.println(sumaBinario(aux));
+	}
+	
+	public static int sumaBinario(String cadena) {
+		StringBuilder cad= new StringBuilder(cadena);
+		cad=cad.reverse();
+		
+		
+	}
+}
